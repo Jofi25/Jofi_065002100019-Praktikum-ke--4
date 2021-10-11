f7ee84e9a5f962e970f0e2dff751d35614c26f53
Jofi25 <91403527+Jofi25@users.noreply.github.com>
2021-10-11 14:57:28 +0700

    Add files via upload

---
+++ praktikum ke 4 tugas ke 1.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct 11 14:42:55 2021
+
+@author: jofi
+"""
+
+
+x = int(input('Masukkan Angka: '))
+for j in range (x,0,-1):
+       for f in range(1,j+1):
+            print(j, end='')
+       print()
+            
